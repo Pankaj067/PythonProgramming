4459c5f304f005dd1e28dac42806daa166054ab7
Pankaj Kumar <Pankaj.Kumar1@mea.ey.net>
2017-12-03 22:54:21 +0530

    Python Learning Code, basic Stuff

---
+++ PythonBasic.py
@@ -0,0 +1,19 @@
+sum = 0
+my_list = [1,2,3]
+for x in my_list:
+	print (x)
+my_list2 = [4,5]
+
+print ("--------------------")
+for i in my_list2:
+	for j in my_list:
+		sum = (sum + j)
+		print (sum)
+print ("******************")
+print (i)
+print (" _ + _ +")
+while  i < 10:
+	i=i+1
+	print (i)
+datainput  = input("Enter the number")
+print (datainput)
