74fc076588ca42794c7c9c2b13cc5e9158a0f9e4
Pankaj Kumar <Pankaj.Kumar1@mea.ey.net>
2017-12-09 03:47:49 +0530

    Download YouTube using Python code

---
+++ YTubeDownload.py
@@ -0,0 +1,24 @@
+import pytube
+
+print("Enter the Video Link")
+link = input()
+
+yt = pytube.YouTube(link)
+videos = yt.get_videos()
+
+s = 1
+for v in videos:
+	print(str(s)+". "+str(v))
+	s += 1
+
+print("Enter the number of the videos")
+n = int(input())
+vid = videos[n-1]
+
+print("Enter the destination")
+destination = input()
+vid.download(destination)
+print("Downloading your video......")
+
+print(yt.filename+"\nDownloaded")
+
