85d608b79099dcf5faa9db24c74dc117fa27fe62
Pankaj Kumar <Pankaj.Kumar1@mea.ey.net>
2017-12-14 11:24:18 +0530

    Google cloud Projects

---
+++ GoogleCloud_API_Lab/GC_VisionToText.py
@@ -0,0 +1,22 @@
+import io
+from google.cloud import vision
+
+vision_client = vision.Client()
+filename = "C:/Users/pankaj.kumar1/Desktop/PythonLib/Images/TestImage.jpg"
+#filename = "C:/Users/pankaj.kumar1/Desktop/PythonLib/Images/Steve.jpg"
+with io.open(filename,'rb') as image_file:
+	content = image_file.read()
+	image = vision_client.image(content = content)
+
+labels = image.detect_labels()
+
+print("-----------------")
+for label in labels:
+	print(label.description , " -> confidence: " , label.score)
+	print("")
+
+print("-----------------")
+
+
+for text in image.detect_text():
+	print(text.description,)
